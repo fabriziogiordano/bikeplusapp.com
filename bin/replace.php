b765fa026fdd9ece0aec52d102bb7049f7b590b0
Fabrizio Giordano <giordano.fabrizio@gmail.com>
2013-08-10 18:43:28 -0400

    replace commands

--- bin/replace.php
+++ bin/replace.php
@@ -4,5 +4,5 @@ $index = str_replace('../../', '../', $index);
 file_put_contents('httpdocs/index.php', $index);
 
 $welcome = file_get_contents('httpdocs/application/controllers/welcome.php');
-$welcome = str_replace('../../', '../', $welcome);
+$welcome = str_replace('../../', '../../subdomains/', $welcome);
 file_put_contents('httpdocs/application/controllers/welcome.php', $welcome);
--- httpdocs/application/controllers/welcome.php
+++ httpdocs/application/controllers/welcome.php
@@ -8,7 +8,7 @@ class Welcome extends CI_Controller {
     if(!$this->agent->is_mobile()) {
       $template['assets'] = array(
         'path' => lang('assets'),
-        'css' => filemtime(APPPATH.'../assets/css/style.desktop.css'),
+        'css' => filemtime(APPPATH.'../../subdomains/assets/css/style.desktop.css'),
       );
       $this->load->view('welcome_browser', $template);
     }
@@ -16,8 +16,8 @@ class Welcome extends CI_Controller {
       $template['json'] = json_encode($this->Apis->latest(lang('site')));
       $template['assets'] = array(
         'path' => lang('assets'),
-        'css'  => filemtime(APPPATH.'../assets/css/style.css'),
-        'js'   => filemtime(APPPATH.'../assets/js/app.js')
+        'css'  => filemtime(APPPATH.'../../subdomains/assets/css/style.css'),
+        'js'   => filemtime(APPPATH.'../../subdomains/assets/js/app.js')
       );
       $this->load->view('welcome', $template);
     }
