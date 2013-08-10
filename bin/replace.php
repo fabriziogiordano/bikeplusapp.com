b03cef5d6b86d68f0d377696ceff137e1218bae5
Fabrizio Giordano <giordano.fabrizio@gmail.com>
2013-08-10 18:41:34 -0400

    replace commands

--- bin/replace.php
+++ bin/replace.php
@@ -2,3 +2,7 @@
 $index = file_get_contents('httpdocs/index.php');
 $index = str_replace('../../', '../', $index);
 file_put_contents('httpdocs/index.php', $index);
+
+$welcome = file_get_contents('httpdocs/application/controllers/welcome.php');
+$welcome = str_replace('../../', '../', $welcome);
+file_put_contents('httpdocs/application/controllers/welcome.php', $welcome);
